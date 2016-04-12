27813510f4b5039b26e87c50d7309c22a0f18e86
Ellen Carbo <ellencarbo@gmail.com>
2016-04-13 00:33:11 +0200

    Update and rename Homework_Day1 to Homework_Day1_Assignment2.sql

---
+++ Homework_Day1_Assignment2.sql
@@ -0,0 +1,23 @@
+# CSND_Advanced_Bfx_Course 
+# Homework_Day1
+
+
+#SQL statement creating two tables, Mutations and Samples
+
+CREATE TABLE Mutations
+(
+MutationID int,
+Chromosome varchar(2),
+Position int,
+Alt varchar(50),
+Ref varchar(50),
+Filter varchar(50),
+Gene varchar(50)
+);
+
+CREATE TABLE Samples
+(
+SampleID varchar(25),
+MutationID int,
+Genotype varchar(3)
+);
